53886cf2de051320df9434f7edb6f10b584916b6
RobertWiltshire <robert@rawiltshire.plus.com>
2020-05-19 11:33:22 +0100

    Changed delay values for LED

--- blink.ino
+++ blink.ino
@@ -12,8 +12,8 @@ void loop()
 {
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(13, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(500); // Wait for 1000 millisecond(s)
   // turn the LED off by making the voltage LOW
   digitalWrite(13, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1500); // Wait for 1000 millisecond(s)
 }
