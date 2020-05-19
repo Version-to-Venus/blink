ab4ebb4af7929fbd716a36f84c53faa77f6a50f9
RobertWiltshire <robert@rawiltshire.plus.com>
2020-05-19 13:44:05 +0100

    added Serial prints and comment

--- blink.ino
+++ blink.ino
@@ -1,19 +1,24 @@
-/*
-  This program blinks pin 13 of the Arduino (the
-  built-in LED)
-*/
+// Assign variables to pins
+const int LED_RED = 12;
+const int LED_GREEN = 13;
 
 void setup()
 {
-  pinMode(13, OUTPUT);
+  Serial.begin(115200);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
 }
 
 void loop()
 {
   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(13, HIGH);
+  digitalWrite(LED_RED, HIGH);
+  digitalWrite(LED_GREEN, LOW);
+  Serial.println("Red on, greenoff");
   delay(250); // Wait for 1000 millisecond(s)
   // turn the LED off by making the voltage LOW
-  digitalWrite(13, LOW);
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_RED, LOW);
+  Serial.println("Red off, green on");
   delay(250); // Wait for 1000 millisecond(s)
 }
